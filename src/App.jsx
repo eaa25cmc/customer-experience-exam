f860ddf5a77f7b9fddbd9e9e5df3cd2b70f20649
eaa25cmc <eaa25cmc@students.eaaa.dk>
2026-03-09 09:36:04 +0100

    sider oprettes og navbar ændret

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,14 @@ import AboutPage from "./pages/AboutPage";
 import ContactPage from "./pages/ContactPage";
 import ServicesPage from "./pages/ServicesPage";
 import NotFoundPage from "./pages/NotFoundPage";
+import SalePage from "./pages/SalePage";
+import ProductsPage from "./pages/ProductsPage";
+import PaymentPage from "./pages/PaymentPage";
+import NewsPage from "./pages/NewsPage";
+import InspirationPage from "./pages/InspirationPage";
+import FavoritesPage from "./pages/FavoritesPage";
+import DetailPage from "./pages/DetailPage";
+import SustainabilityPage from "./pages/SustainabilityPage";
 
 export default function App() {
   return (
@@ -16,7 +24,15 @@ export default function App() {
         <Route path="/about" element={<AboutPage />} />
         <Route path="/contact" element={<ContactPage />} />
         <Route path="/services" element={<ServicesPage />} />
+        <Route path="/sale" element={<SalePage />} />
+        <Route path="/products" element={<ProductsPage />} />
+        <Route path="/payment" element={<PaymentPage />} />
+        <Route path="/news" element={<NewsPage />} />
+        <Route path="/inspiration" element={<InspirationPage />} />
+        <Route path="/favorites" element={<FavoritesPage />} />
+        <Route path="/detail" element={<DetailPage />} />
         <Route path="*" element={<NotFoundPage />} />
+        <Route path="/sustainability" element={<SustainabilityPage />} />
       </Routes>
       <Footer />
     </>
---
+++ src/pages/SalePage.jsx
@@ -0,0 +1,30 @@
+export default function SalePage() {
+  return (
+    <>
+      <header>
+        <h1>Udsalg</h1>
+      </header>
+      <main className="about-container">
+        <p>
+          We build modern web apps with React, clear branching, and structured
+          Pull Requests.
+        </p>
+
+        <section className="about-stats">
+          <article className="stat">
+            <h3>12+</h3>
+            <p>Projects</p>
+          </article>
+          <article className="stat">
+            <h3>4</h3>
+            <p>Team Members</p>
+          </article>
+          <article className="stat">
+            <h3>100%</h3>
+            <p>Deployed</p>
+          </article>
+        </section>
+      </main>
+    </>
+  );
+}
