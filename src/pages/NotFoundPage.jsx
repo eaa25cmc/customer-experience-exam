03f7fe621cf20152b89311edbb9695b551541b51
lauralynge <lauralynge@gmail.com>
2026-03-13 02:11:23 +0100

    Rettelser i favorit-side, kurv-overlay og footer

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -7,9 +7,9 @@ export default function NotFoundPage() {
         <h1 className="not-found-title">404</h1>
       </header>
       <main className="not-found">
-        <p>Sorry, the page you're looking for doesn't exist.</p>
+        <p>Siden du leder efter findes ikke.</p>
         <Link to="/" className="not-found-link">
-          Go back home
+          Gå tilbage til forsiden
         </Link>
       </main>
     </>
